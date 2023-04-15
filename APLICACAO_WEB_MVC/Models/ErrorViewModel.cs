fe691f875b4c304ce8fd4371ce39a6225b974dc1
profe <profe@DESKTOP-ANVV0SF>
2023-04-15 19:01:33 -0300

    Adicionar arquivos de projeto.

---
+++ APLICACAO_WEB_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APLICACAO_WEB_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
